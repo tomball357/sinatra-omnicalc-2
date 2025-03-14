ca48e9f2624f013385674e1133640f486bb5cd5d
tomball357 <tball@chicagobooth.edu>
2025-03-14 17:28:23 +0000

    sync

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @second_num - @first_num
   erb(:sub_result)
 end
 
@@ -32,13 +33,25 @@ get("/multiply") do
 end
 
 get("/wizard multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num ** @second_num
+
   erb(:mult_result)
 end
 
 get("/divide") do
+
   erb(:div_form)
 end
 
 get("/wizard_divide") do
+
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
+
   erb(:div_result)
 end
